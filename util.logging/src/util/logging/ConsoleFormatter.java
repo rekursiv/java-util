1c2cbb33e0b76f42ea5625927af4fc258d0d046a
Aaron Cram <sergyigarwa@gmail.com>
2014-02-20 12:39:34 -0700

    added custom formatter so timestamp can be seen in console

---
+++ util.logging/src/util/logging/ConsoleFormatter.java
@@ -0,0 +1,25 @@
+package util.logging;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Formatter;
+import java.util.logging.LogRecord;
+
+public class ConsoleFormatter extends Formatter {
+
+	 private static final DateFormat df = new SimpleDateFormat("MM/dd|hh:mm:ss.SSS");
+	
+	@Override
+	public String format(LogRecord record) {
+        StringBuilder builder = new StringBuilder(1000);
+        builder.append("["+df.format(new Date(record.getMillis()))+"]");
+        builder.append(" \""+formatMessage(record)+"\"");
+        builder.append(" {"+record.getLevel()+":");
+        builder.append(record.getSourceClassName()+"#");
+        builder.append(record.getSourceMethodName()+"}  ");
+        builder.append("\n");
+		return builder.toString();
+	}
+
+}
--- util.logging/src/util/logging/LogSetup.java
+++ util.logging/src/util/logging/LogSetup.java
@@ -4,7 +4,6 @@ import java.util.logging.ConsoleHandler;
 import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.LogManager;
-import java.util.logging.SimpleFormatter;
 
 
 public class LogSetup {
@@ -20,12 +19,12 @@ public class LogSetup {
 	}
 	
 	public static void initConsole(Level level) {
-		System.getProperties().setProperty("java.util.logging.SimpleFormatter.format", "%5$s  [%4$s] {%2$s} %6$s%n");   //  FIXME:  write custom formatter
 		Handler ch = new ConsoleHandler();
-		ch.setFormatter(new SimpleFormatter());
+		ch.setFormatter(new ConsoleFormatter());
 		ch.setLevel(level);
 		LogManager.getLogManager().getLogger("").addHandler(ch);
 	}
 	
 
 }
+
